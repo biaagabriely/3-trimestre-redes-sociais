5b49af9e51ea8b0991c95cdb0b12713ea72b2a73
biaagabriely <127507324+biaagabriely@users.noreply.github.com>
2024-10-25 08:59:20 -0300

    ,

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -13,7 +13,7 @@ async function visualizarInfomacoesGlobais() {
     paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas}</span> horas e <span>${minutos}</span> minutos conectadas. 
     <br> Isso significa que aproximadamente  ${porcentagemConectada}% de pessoas estão conectadas em alguma rede social.`
     const container = document.getElementById('graficos-container')
-    container.appendChild(paragrafo)
+    container.appendChild(paragrafo) 
        
 }
 visualizarInfomacoesGlobais()
